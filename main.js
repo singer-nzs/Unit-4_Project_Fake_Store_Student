f71d6ba2fe304ef357dcb2c092cab3ef0e54ad00
SinGer ZS <singer.nzs@gmail.com>
2023-10-13 18:17:21 -0400

    Ticket #U4_03 Obtain Data --Code Review

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 //! Global Variables
-const baseURL = `https://fakestoreapi.com/products/1`;
 const electronicsLink = document.getElementById("electronics");
 const jeweleryLink = document.getElementById("jewelery");
 const menclothingLink = document.getElementById("mens-clothing");
 const womenclothingLink = document.getElementById("womens-clothing");
 const display = document.getElementById("display");
+const baseURL = `https://fakestoreapi.com/products/1`;
 
 //! Functions
+async function fakeStore (endpoint) {
+    try {
+    const response = await fetch(`${baseURL}/${endpoint}`);
+        console.log(response);
+    } catch {
+        (err => console.error(err));
+    };
+};
