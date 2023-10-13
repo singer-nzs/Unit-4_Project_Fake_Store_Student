33e2c9222d6c078c96953c66152b2df9e46007c6
SinGer ZS <singer.nzs@gmail.com>
2023-10-13 17:37:08 -0400

    Ticket #U4_03 Global Variables

---
+++ main.js
@@ -0,0 +1,11 @@
+//! Global Variables
+const baseURL = `https://fakestoreapi.com/products/1`;
+const electronicsLink = document.getElementById("electronics");
+const jeweleryLink = document.getElementById("jewelery");
+const menclothingLink = document.getElementById("mens-clothing");
+const womenclothingLink = document.getElementById("womens-clothing");
+const display = document.getElementById("display");
+
+let storedRecipes = [];
+
+const searchForm = document.getElementById('search-form');
